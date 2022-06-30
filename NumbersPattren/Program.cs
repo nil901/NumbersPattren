1cf0c6e8c4b94707ab535167a17dca6969cfb942
Nilesh <pathaknilesh1998@gmail.com>
2022-06-30 12:33:01 +0530

    Add project files.

---
+++ NumbersPattren/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersPattren
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++) {
+            //        Console.Write(i); 
+
+            //    }
+            //    Console.WriteLine();  
+            //} 
+            //Console.ReadLine();
+
+            int count = 0;
+            for (int i = 1; i <= 4; i++) 
+            {
+                for (int j = 1; j <= i; j++)
+                { 
+                    count++;
+                    Console.Write(count);
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
