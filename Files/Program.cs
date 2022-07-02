c799712fb6f68036a079fe25f15748b15440dae5
Nilesh <pathaknilesh1998@gmail.com>
2022-07-02 12:52:30 +0530

    File

---
+++ Files/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Files
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+              // include the System.IO namespace
+
+            //string writeText = "Hello World!";  // Create a text string
+            //File.WriteAllText("F:\\FileText.txt  ", writeText);  // Create a file and write the content of writeText to it
+
+            string readText = File.ReadAllText("F:\\FileText.txt");  // Read the contents of the file
+            Console.WriteLine(readText);
+            Console.ReadLine();// Output the content
+        }
+    }
+}
