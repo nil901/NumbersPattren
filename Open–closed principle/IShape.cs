daafaf1cf262a688215326318ff607368919d534
Nilesh <pathaknilesh1998@gmail.com>
2022-07-02 11:51:41 +0530

    Open-close-principle

---
+++ Open–closed principle/IShape.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_closed_principle
+{
+    public interface IShape
+    {
+        //This interface represent Open Modification /Extension 
+
+        public double Area();
+    }
+
+    public class CalculateArea
+    {
+        public double CalculateArea(IShape shape)
+        { 
+           // close for modification
+            return shape.Area;
+        }
+    } 
+
+        public class Circle : IShape
+        {
+            const double PI = 3.14;
+
+            double radius;
+
+            public Circle(double radius)
+            {
+                this.radius = radius;
+            }
+        public double Area => 2 * PI * radius;
+
+    }
+
+        public class Rectangle : IShape
+        {
+            double length;
+
+            double width;
+
+
+            public Rectangle(double length, double width)
+            {
+                this.length = length;
+                this.width = width;
+            }
+
+
+   
+
+        double IShape.Area()
+        {
+            return length * width;
+        }
+    }
+    class OpenClosePrincipal : IShape
+    {
+        CalculateArea calculate = new CalculateArea();
+        Circle circle = new Circle(3);
+
+        Rectangle rectangle = new Rectangle(5,6);
+
+        public double Area()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    }
+
---
+++ Open–closed principle/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_closed_principle
+{
+    public class Shape
+    {
+        string shape;
+        const double PI = 3.14; 
+
+        public Shape(string shape)
+        {
+            this.shape = shape; 
+        }
+        public double Area()
+        {
+            double area = 0;
+
+            if (this.shape.ToLower().Contains("circle"))
+            {
+                Console.WriteLine("Enter the radius ");
+                string radius = Console.ReadLine();
+                area = 2 * PI * Convert.ToInt64(radius);
+            } else if (this.shape.ToLower().Contains("rectangle")) {
+                Console.WriteLine("Enter the Length ");
+                string Length = Console.ReadLine();
+                Console.WriteLine("Enter the Width ");
+                string Width = Console.ReadLine();
+                area = Convert.ToInt64(Length) * Convert.ToInt64(Width);
+
+            }
+            return area;
+        }
+
+    } 
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Shape shape = new Shape("rectangle");
+            //Console.WriteLine("Area Circle of" + shape.Area());
+
+            Shape shape = new Shape("circle");
+            Console.WriteLine("Area Circle of" + shape.Area());
+            Console.ReadLine();
+        }
+    }
+}
