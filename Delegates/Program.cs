135e8ec2477cff8ed291eef22f5dac16447f59d5
Nilesh <pathaknilesh1998@gmail.com>
2022-07-02 16:57:21 +0530

    Mytask

---
+++ Delegates/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+     public delegate void calculation(int a ,int b);
+  
+    public  class Program
+    { 
+        public static void Addition(int a , int b)
+        {
+            int result = a + b;
+            Console.WriteLine("Addition result is {0}" , result); 
+        }
+        public static void subtraction(int a, int b)
+        {
+            int result = a - b;
+            Console.WriteLine("subtraction result is {0}", result);
+        }
+        public static void multiplication(int a, int b)
+        {
+            int result = a * b;
+            Console.WriteLine("multiplication result is {0}", result);
+        }
+        public static void division(int a, int b)
+        {
+            int result = a / b;
+            Console.WriteLine("division result is {0}", result);
+        }
+       
+        static void Main(string[] args)
+        {
+            calculation obj = new calculation(Program.Addition);
+            calculation obj1 = new calculation(Program.multiplication);
+            obj1(5, 6); 
+            
+            //obj.Invoke(20,20);
+            //obj = multiplication;
+            //obj(20,50);         // point dffrent function 
+            //obj = division;
+            //obj(7 , 5);
+          Console.ReadLine();  
+                
+
+        }
+    }
+}
