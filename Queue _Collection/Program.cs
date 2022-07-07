fd6d0e060687e0c9d2e3a3787456c75876a49e15
Nilesh <pathaknilesh1998@gmail.com>
2022-07-07 19:03:33 +0530

    Nil

---
+++ Queue _Collection/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue__Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<int> que = new Queue<int>();    //stores the elements in FIFO style
+            que.Enqueue(1);
+            que.Enqueue(2);
+            que.Enqueue(3);
+            que.Enqueue(4);
+            que.Enqueue(5);
+
+            que.Contains(4);
+
+            foreach(var id in que){
+                Console.WriteLine(id);
+            }
+
+     
+
+
+
+            Queue<string> strQ = new Queue<string>();
+            strQ.Enqueue("N");
+            strQ.Enqueue("I");
+            strQ.Enqueue("L");
+            strQ.Enqueue("E");
+            strQ.Enqueue("S");
+            strQ.Enqueue("H");
+
+            Console.WriteLine("Total elements: {0}", strQ.Count); 
+
+            while (strQ.Count > 0)
+                Console.WriteLine(strQ.Dequeue()); 
+
+            Console.WriteLine("Total elements: {0}", strQ.Count);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Stack_Collection/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> mystack = new Stack<int>(); //stores elements in LIFO style
+            mystack.Push(1);
+            mystack.Push(2);
+            mystack.Push(3);
+            mystack.Push(4);
+            mystack.Push(5);
+
+            //foreach (var item in mystack) {
+            //    Console.WriteLine(item + "  ");
+
+            //}
+
+
+
+
+            // create a Stack from an array, 
+
+            //int[] arr = new int[] { 1, 2, 3, 4, 5, 6,7, 8, 9 };
+            //Stack<int> ST = new Stack<int>(arr);
+
+            //foreach (var item in ST)
+            //{
+            //    Console.WriteLine(item + "" );
+            //} 
+
+
+           
+
+
+        }
+    }
+}
