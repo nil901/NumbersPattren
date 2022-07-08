05fd60e477ee1eeba2265f171517786637cb0b74
Nilesh <pathaknilesh1998@gmail.com>
2022-07-08 16:33:57 +0530

    hii

---
+++ Jump_ statement/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jump__statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+
+                //if( i == 0)
+                //{
+                //    //break; b     //break statment
+
+                //}  
+
+                //if(i % 2 == 1) {
+
+                //    continue;     // continue statment 
+
+           // }
+                if (i== 5) {
+                    goto stop;
+                  }
+                
+
+               Console.WriteLine(i);
+            }
+            
+
+            Console.WriteLine("Loop Terminate ");
+
+            stop:
+            Console.WriteLine("Program exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ params/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @params
+{
+    class Program
+    {
+
+        //public static int Add(int a , int b)
+        //{
+        //   int  sum = a + b;
+        //    return sum; 
+
+
+        //}   
+
+        public static int add(params int[] nums)
+        {
+            int sum = 0;
+            foreach (var i in  nums )
+            {
+                sum = sum + i;
+            }
+            return sum;
+        }
+        static void Main(string[] args){
+
+            Console.WriteLine(Program.add(10,10,10,10,10,10,10,10,10,10,10,10));    
+            Console.ReadLine(); 
+            
+        } 
+
+    }
+}
